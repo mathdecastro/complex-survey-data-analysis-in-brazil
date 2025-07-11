1af3703160b269e4747ba7eb9ad965e51456afd8
mathdecastro <theusrjxd@gmail.com>
2025-07-10 22:59:16 -0300

    :books: docs: adding a folder for the images from the dashboard and updating app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 from pnadc import mapping_dataframe, pnad_dataframe
 import streamlit as st
+import pandas as pd
 
 # TITLE
-st.title('PNADC Dashboard')
+st.sidebar.title('PNADC Dashboard')
 
 # CREATING MAPPING DATAFRAME
 dictionary_url = 'https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_continua/Trimestral/Microdados/Documentacao/Dicionario_e_input_20221031.zip'
@@ -11,16 +12,16 @@ variables_filename = 'https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nac
 df_mapping = mapping_dataframe(dictionary_url = dictionary_url, dictionary_filename = dictionary_filename, variables_filename = variables_filename)
 
 # SELECTING THE YEAR, QUARTER AND VARIABLES
-select_year = st.selectbox('Select the year of the survey: ', [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025])
-select_quarter = st.selectbox('Select the quarter of the survey: ', [1, 2, 3, 4])
+select_year = st.sidebar.selectbox('Select the year of the survey: ', [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025])
+select_quarter = st.sidebar.selectbox('Select the quarter of the survey: ', [1, 2, 3, 4])
 try:
-    select_variables = st.multiselect('Select the variables of the survey: ', df_mapping[(df_mapping['survey_period'] == f'{select_quarter}º tri/{select_year}') & (df_mapping['in_the_survey'] == 1)]['variable'].unique(), default = ['Ano', 'Trimestre', 'UF', 'Estrato', 'V1028'])
+    select_variables = st.sidebar.multiselect('Select the variables of the survey: ', df_mapping[(df_mapping['survey_period'] == f'{select_quarter}º tri/{select_year}') & (df_mapping['in_the_survey'] == 1)]['variable'].unique(), default = ['Ano', 'Trimestre', 'UF', 'Estrato', 'V1028'])
 except Exception as e:
-    st.exception(Exception('There is no survey for this year or quarter'))
+    st.sidebar.exception(Exception('There is no survey for this year or quarter'))
 
 # DOWNLOAD DATA
-if st.button('Download data'):
-    with st.spinner('Downloading data'):
+if st.sidebar.button('Create dashboard'):
+    with st.spinner('Downloading data and creating dashboard.'):
         df = pnad_dataframe(year = select_year, quarter = select_quarter, variables = select_variables, df_mapping = df_mapping)
 
     # HEADER
@@ -64,4 +65,7 @@ if st.button('Download data'):
     df_employment.columns = ['state', 'region', 'unemployment_rate']
 
     # SHOWING THE DATAFRAME
-    st.bar_chart(df_employment, x = 'state', y = ['unemployment_rate'], color = 'region')
+    st.bar_chart(df_employment, x = 'state', y = ['unemployment_rate'], color = 'region', x_label = 'State', y_label = 'Unemployment Rate (%)')
+
+    st.markdown('#### :grey[Ranking]')
+    st.table(df_employment.sort_values(by = ['unemployment_rate'], ascending = False).set_index(pd.RangeIndex(1, len(df_employment) + 1)))
